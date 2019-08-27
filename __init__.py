42897b2882bcea22c459c4c92c223adb3e74d119
Matias Carrasco Kind <mgckind@gmail.com>
2019-08-27 11:07:36 -0500

    update init file

---
+++ __init__.py
@@ -0,0 +1,7 @@
+""" flimview: small set of utilities to access and analyse FLIM data"""
+__author__ = "Matias Carrasco Kind"
+__license__ = "NCSA"
+__version__ = "1.0.0"
+
+from . import flim, io_utils, models, plot_utils
+__all__ = ["flim", "io_utils", "models", "plot_utils"]
