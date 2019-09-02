e683c2344e895962bd9b21a938adcd6c3d38197c
Matias Carrasco Kind <mgckind@gmail.com>
2019-09-02 18:34:26 -0500

    add Readme:

--- __init__.py
+++
@@ -1,7 +0,0 @@
-""" flimview: small set of utilities to access and analyse FLIM data"""
-__author__ = "Matias Carrasco Kind"
-__license__ = "NCSA"
-from .version import __version__
-
-from . import flim, io_utils, models, plot_utils
-__all__ = ["flim", "io_utils", "models", "plot_utils"]
--- version.py
+++
@@ -1,7 +0,0 @@
-"""flimview: A Lightweight python package for FLIM image processing"""
-
-version_tag = (1, 0, 0)
-__version__ = '.'.join(map(str, version_tag[:3]))
-
-if len(version_tag) > 3:
-    __version__ = '%s-%s' % (__version__, version_tag[3])
