42e44af364d18f2aa656e2ff8b6450edde90879c
Matias Carrasco Kind <mgckind@gmail.com>
2019-09-02 18:42:38 -0500

    update package

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+import os
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+prjdir = os.path.dirname(__file__)
+
+def read(filename):
+    return open(os.path.join(prjdir, filename)).read()
+
+extra_link_args = []
+libraries = []
+library_dirs = []
+include_dirs = []
+exec(open('flimview/version.py').read())
+setup(
+    name='flimview',
+    version=__version__,
+    author='Matias Carrasco Kind , COMI Lab',
+    author_email='mcarras2@illinois.edu',
+    scripts=[],
+    packages=['flimview'],
+    license='License.txt',
+    description='Python ligtweight flim image processing',
+    long_description=read('README.md'),
+    url='https://github.com/sahandha/eif',
+    install_requires=["numpy","scipy", "pandas", "matplotlib", "sdtfile","json", "h5py" ],
+)
