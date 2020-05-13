729bbde9824eb2899d574766c65045a1d29ab657
Matias Carrasco Kind <mgckind@gmail.com>
2020-05-13 14:00:27 -0500

    update setup and version

--- flimview/version.py
+++ flimview/version.py
@@ -1,6 +1,6 @@
 """flimview: A Lightweight python package for FLIM image processing"""
 
-version_tag = (1, 1, 0)
+version_tag = (1, 2, 0)
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
